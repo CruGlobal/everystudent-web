4fea79c879c63f74fcfb81288f20669893f4de75
Josh Starcher <josh.starcher@gmail.com>
2018-01-25 12:07:45 -0500

    Fix docker tag.

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
-`aws ecr get-login --no-include-email --region us-east-1`
-docker build -t 056154071827.dkr.ecr.us-east-1.amazonaws.com/$PROJECT_NAME:$GIT_COMMIT-$BUILD_NUMBER .
+docker build -t 056154071827.dkr.ecr.us-east-1.amazonaws.com/$PROJECT_NAME:$ENVIRONMENT-$BUILD_NUMBER .
 
